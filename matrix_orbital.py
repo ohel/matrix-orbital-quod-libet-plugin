c9d16f7cefd78e8fb6191a176ff8ae4a3d3a7b33
ohel <ohel@users.noreply.github.com>
2018-10-23 00:27:12 +0300

    Refactor and cleanup.

--- matrix_orbital.py
+++ matrix_orbital.py
@@ -38,273 +38,277 @@ class Config(object):
 CONFIG = Config()
 
 
+class Alignment(Enum):
+
+    LEFT = 1
+    RIGHT = 2
+    CENTER = 3
+    TOP = 4
+    BOTTOM = 5
+
+
+class Phase(Enum):
+
+    BASIC_INFO = 1
+    BASIC_SCROLL = 2
+    BASIC_INFO_2 = 3
+    BASIC_SCROLL_2 = 4
+    DISC_INFO = 5
+    HEADER_INFO = 6
+
+
+class NowPlayingLCDData(object):
+
+    def __init__(self, parent):
+
+        self._parent = parent
+        self._max_width = int(CONFIG.lcd_width)
+        self._interval = float(CONFIG.lcd_interval)
+        self._ticks_in_phase = self._seconds_to_ticks(4)
+        self.reset()
+
+    def set_basic_info(self, artist, title):
+
+        self._artist = unidecode(artist)
+        self._title = unidecode(title)
+
+        # If the text should scroll, pad it to scroll width.
+        if len(self._artist) > self._max_width:
+            self._artist += "".ljust(self._max_width)
+        if len(self._title) > self._max_width:
+            self._title += "".ljust(self._max_width)
+
+    def set_disc_info(self, album, discnumber, tracknumber):
+
+        self._disc_info_row_1 = ""
+        self._disc_info_row_2 = ""
+
+        if album is not None:
+            tlalbum = unidecode(album)
+            if len(tlalbum) > self._max_width:
+                tlalbum = tlalbum[0:self._max_width - 3] + "..."
+            self._disc_info_row_1 = tlalbum
+
+        if discnumber is not None:
+            # The 9 in ljust comes from a string such as "Disc 1/9 ".
+            self._disc_info_row_2 = \
+                ("Disc " + unidecode(discnumber) + " ").ljust(9)
+
+        if tracknumber is not None:
+            self._disc_info_row_2 += ("Track " + unidecode(tracknumber)) \
+                .rjust(self._max_width - len(self._disc_info_row_2))
+
+    def reset(self):
+
+        self._artist = None
+        self._title = None
+        self._disc_info_row_1 = ""
+        self._disc_info_row_2 = ""
+        self._skip_ticks = 0
+        self._scroll_a = 0
+        self._scroll_t = 0
+        self._tick_count = 0
+        self._phase = Phase.BASIC_INFO
+        self._force_refresh = True
+
+    def prevent_update(self, seconds):
+
+        if seconds < 0:
+            self._skip_ticks = -1
+        else:
+            self._skip_ticks = self._seconds_to_ticks(seconds)
+
+    def set_forced_update(self):
+
+        self._force_refresh = True
+
+    def _seconds_to_ticks(self, seconds):
+
+        return int(round(seconds * (1000 / self._interval)))
+
+    def _advance_phase(self):
+
+        self._scroll_a = 0
+        self._scroll_t = 0
+        self._tick_count = 0
+
+        # Don't scroll if there is no need to.
+        if (len(self._artist) <= self._max_width):
+            self._scroll_a = -1
+        if (len(self._title) <= self._max_width):
+            self._scroll_t = -1
+
+        next_phases = {
+            Phase.BASIC_INFO: Phase.BASIC_SCROLL,
+            Phase.BASIC_SCROLL: Phase.BASIC_INFO_2,
+            Phase.BASIC_INFO_2: Phase.BASIC_SCROLL_2,
+            Phase.BASIC_SCROLL_2: Phase.DISC_INFO,
+            Phase.DISC_INFO: Phase.HEADER_INFO,
+            Phase.HEADER_INFO: Phase.BASIC_INFO
+            }
+        self._phase = next_phases.get(self._phase, Phase.HEADER_INFO)
+
+        self._force_refresh = True
+
+    def on_tracker_tick(self, tracker):
+
+        if (self._artist is None or
+            self._title is None or
+            self._skip_ticks < 0):
+            return
+
+        if self._skip_ticks > 0:
+            self._skip_ticks -= 1
+            # If resuming after skipped ticks, force refresh.
+            # This makes sure there is no garbage on the display.
+            if self._skip_ticks == 0:
+                self._force_refresh = True
+            return
+
+        self._tick_count += 1
+
+        # Display static basic info.
+        if (self._phase == Phase.BASIC_INFO or
+            self._phase == Phase.BASIC_INFO_2):
+
+            if (self._force_refresh):
+                self._force_refresh = False
+                self._parent._reset_lcd()
+                self._refresh_info()
+
+            if (self._tick_count > self._ticks_in_phase):
+                self._advance_phase()
+
+            return
+
+        # Display scrolling basic info.
+        if (self._phase == Phase.BASIC_SCROLL or
+            self._phase == Phase.BASIC_SCROLL_2):
+
+            # Value -1 indicates scrolling is done.
+            refresh_artist = self._scroll_a > -1 or self._force_refresh
+            refresh_title = self._scroll_t > -1 or self._force_refresh
+
+            if (self._tick_count > self._ticks_in_phase and
+                self._scroll_a == -1 and self._scroll_t == -1):
+                self._advance_phase()
+                return
+
+            if self._force_refresh:
+                self._force_refresh = False
+                self._parent._reset_lcd()
+
+            if self._scroll_a > -1:
+                self._scroll_a += 1
+                if self._scroll_a > len(self._artist):
+                    self._scroll_a = -1
+
+            if self._scroll_t > -1:
+                self._scroll_t += 1
+                if self._scroll_t > len(self._title):
+                    self._scroll_t = -1
+
+            if refresh_artist:
+                self._refresh_info(True, False)
+            if refresh_title:
+                self._refresh_info(False)
+
+            return
+
+        # Display disc info.
+        if self._phase == Phase.DISC_INFO:
+
+            if self._force_refresh:
+                self._force_refresh = False
+                self._parent._reset_lcd()
+                self._refresh_disc_info()
+
+            if self._tick_count > self._ticks_in_phase:
+                self._advance_phase()
+
+            return
+
+        # Display disc info.
+        if self._phase == Phase.HEADER_INFO:
+
+            if self._force_refresh:
+                self._force_refresh = False
+                self._parent._reset_lcd()
+                self._parent._write_header_with_text("* now playing *")
+
+            if self._tick_count > self._ticks_in_phase / 2:
+                self._advance_phase()
+
+            return
+
+    def _write_text(self, text, scroll, v_align):
+
+        if scroll is not None and scroll > 0:
+            text = (text[scroll:] + text)
+        text = text[0:self._max_width]
+        self._parent.write_bytes(
+            self._parent.align_text2bytes(text, Alignment.LEFT, v_align))
+
+    def _refresh_disc_info(self):
+
+        self._write_text(self._disc_info_row_1, None, Alignment.TOP)
+        self._write_text(self._disc_info_row_2, None, Alignment.BOTTOM)
+
+    def _refresh_info(self, artist=True, title=True, disc_info=False):
+
+        if artist:
+            self._write_text(self._artist, self._scroll_a, Alignment.TOP)
+
+        if title:
+            self._write_text(self._title, self._scroll_t, Alignment.BOTTOM)
+
+
 class MatrixOrbitalLCD(EventPlugin):
 
     PLUGIN_ID = 'MatrixOrbitalLCD'
     PLUGIN_NAME = _('Matrix Orbital LCD')
     PLUGIN_DESC = _("Print info to a Matrix Orbital MX2/MX3 LCD.")
     PLUGIN_ICON = Icons.UTILITIES_TERMINAL
-    PLUGIN_VERSION = '0.1'
+    PLUGIN_VERSION = '1.0'
 
-    class _Alignment(Enum):
+    def align_text2bytes(self, text,
+        h_align=Alignment.CENTER, v_align=Alignment.TOP):
 
-        LEFT = 1
-        RIGHT = 2
-        CENTER = 3
-        TOP = 4
-        BOTTOM = 5
-
-    class _NowPlayingLCDData(object):
-
-        class _Phase(Enum):
-
-            BASIC_INFO = 1
-            BASIC_SCROLL = 2
-            BASIC_INFO_2 = 3
-            BASIC_SCROLL_2 = 4
-            DISC_INFO = 5
-            HEADER_INFO = 6
-
-        def __init__(self, parent):
-
-            self._parent = parent
-            self._max_width = int(CONFIG.lcd_width)
-            self._interval = float(CONFIG.lcd_interval)
-            self._ticks_in_phase = self._seconds_to_ticks(4)
-            self.reset()
-
-        def set_basic_info(self, artist, title):
-
-            self._artist = unidecode(artist)
-            self._title = unidecode(title)
-
-            # If the text should scroll, pad it to scroll width.
-            if len(self._artist) > self._max_width:
-                self._artist += "".ljust(self._max_width)
-            if len(self._title) > self._max_width:
-                self._title += "".ljust(self._max_width)
-
-        def set_disc_info(self, album, discnumber, tracknumber):
-
-            self._disc_info_row_1 = ""
-            self._disc_info_row_2 = ""
-
-            if album is not None:
-                tlalbum = unidecode(album)
-                if len(tlalbum) > self._max_width:
-                    tlalbum = tlalbum[0:self._max_width - 3] + "..."
-                self._disc_info_row_1 = tlalbum
-
-            if discnumber is not None:
-                self._disc_info_row_2 = \
-                    ("Disc " + unidecode(discnumber) + " ").ljust(9)
-
-            if tracknumber is not None:
-                self._disc_info_row_2 += ("Track " + unidecode(tracknumber)) \
-                    .rjust(self._max_width - len(self._disc_info_row_2))
-
-        def reset(self):
-
-            self._artist = None
-            self._title = None
-            self._disc_info_row_1 = ""
-            self._disc_info_row_2 = ""
-            self._skip_ticks = 0
-            self._scroll_a = 0
-            self._scroll_t = 0
-            self._tick_count = 0
-            self._phase = self._Phase.BASIC_INFO
-            self._force_refresh = True
-
-        def prevent_update(self, seconds):
-
-            if seconds < 0:
-                self._skip_ticks = -1
-            else:
-                self._skip_ticks = self._seconds_to_ticks(seconds)
-
-        def set_forced_update(self):
-
-            self._force_refresh = True
-
-        def _seconds_to_ticks(self, seconds):
-
-            return int(round(seconds * (1000 / self._interval)))
-
-        def _advance_phase(self):
-
-            self._scroll_a = 0
-            self._scroll_t = 0
-            self._tick_count = 0
-
-            # Don't scroll if there is no need to.
-            if (len(self._artist) <= self._max_width):
-                self._scroll_a = -1
-            if (len(self._title) <= self._max_width):
-                self._scroll_t = -1
-
-            next_phases = {
-                self._Phase.BASIC_INFO: self._Phase.BASIC_SCROLL,
-                self._Phase.BASIC_SCROLL: self._Phase.BASIC_INFO_2,
-                self._Phase.BASIC_INFO_2: self._Phase.BASIC_SCROLL_2,
-                self._Phase.BASIC_SCROLL_2: self._Phase.DISC_INFO,
-                self._Phase.DISC_INFO: self._Phase.HEADER_INFO,
-                self._Phase.HEADER_INFO: self._Phase.BASIC_INFO
-                }
-            self._phase = next_phases.get(self._phase, self._Phase.HEADER_INFO)
-
-            self._force_refresh = True
-
-        def on_tracker_tick(self, tracker):
-
-            if (self._artist is None or
-                self._title is None or
-                self._skip_ticks < 0):
-                return
-
-            if self._skip_ticks > 0:
-                self._skip_ticks -= 1
-                # If resuming after skipped ticks, force refresh.
-                # This makes sure there is no garbage on the display.
-                if self._skip_ticks == 0:
-                    self._force_refresh = True
-                return
-
-            self._tick_count += 1
-
-            # Display static basic info.
-            if (self._phase == self._Phase.BASIC_INFO or
-                self._phase == self._Phase.BASIC_INFO_2):
-
-                if (self._force_refresh):
-                    self._force_refresh = False
-                    self._parent._reset_lcd()
-                    self._refresh()
-
-                if (self._tick_count > self._ticks_in_phase):
-                    self._advance_phase()
-
-                return
-
-            # Display scrolling basic info.
-            if (self._phase == self._Phase.BASIC_SCROLL or
-                self._phase == self._Phase.BASIC_SCROLL_2):
-
-                # Value -1 indicates scrolling is done.
-                refresh_artist = self._scroll_a > -1 or self._force_refresh
-                refresh_title = self._scroll_t > -1 or self._force_refresh
-
-                if (self._tick_count > self._ticks_in_phase and
-                    self._scroll_a == -1 and self._scroll_t == -1):
-                    self._advance_phase()
-                    return
-
-                if self._force_refresh:
-                    self._force_refresh = False
-                    self._parent._reset_lcd()
-
-                if self._scroll_a > -1:
-                    self._scroll_a += 1
-                    if self._scroll_a > len(self._artist):
-                        self._scroll_a = -1
-
-                if self._scroll_t > -1:
-                    self._scroll_t += 1
-                    if self._scroll_t > len(self._title):
-                        self._scroll_t = -1
-
-                if refresh_artist:
-                    self._refresh(True, False)
-                if refresh_title:
-                    self._refresh(False, True)
-
-                return
-
-            # Display disc info.
-            if self._phase == self._Phase.DISC_INFO:
-
-                if self._force_refresh:
-                    self._force_refresh = False
-                    self._parent._reset_lcd()
-                    self._refresh(False, False, True)
-
-                if self._tick_count > self._ticks_in_phase:
-                    self._advance_phase()
-
-                return
-
-            # Display disc info.
-            if self._phase == self._Phase.HEADER_INFO:
-
-                if self._force_refresh:
-                    self._force_refresh = False
-                    self._parent._reset_lcd()
-                    self._parent._write_header_with_text("* now playing *")
-
-                if self._tick_count > self._ticks_in_phase / 2:
-                    self._advance_phase()
-
-                return
-
-        def _write_text(self, text, scroll, v_align):
-
-            if scroll is not None and scroll > 0:
-                text = (text[scroll:] + text)
-            text = text[0:self._max_width]
-            self._parent._dev.write(self._parent._align_text(text,
-                self._parent._Alignment.LEFT, v_align))
-
-        def _refresh(self, artist=True, title=True, disc_info=False):
-
-            if disc_info:
-                self._write_text(
-                    self._disc_info_row_1, None, self._parent._Alignment.TOP)
-                self._write_text(self._disc_info_row_2,
-                    None, self._parent._Alignment.BOTTOM)
-                return
-
-            if artist:
-                self._write_text(
-                    self._artist, self._scroll_a, self._parent._Alignment.TOP)
-
-            if title:
-                self._write_text(self._title, self._scroll_t,
-                    self._parent._Alignment.BOTTOM)
-
-    def _align_text(self, text,
-        h_align=_Alignment.CENTER, v_align=_Alignment.TOP):
-
-        if (h_align == self._Alignment.LEFT):
+        if (h_align == Alignment.LEFT):
             index = 1
-        elif (h_align == self._Alignment.RIGHT):
+        elif (h_align == Alignment.RIGHT):
             index = max(CONFIG.lcd_width - len(text) + 1, 1)
-        elif (h_align == self._Alignment.CENTER):
+        elif (h_align == Alignment.CENTER):
             index = floor((int(CONFIG.lcd_width) - len(text)) / 2) + 1
 
-        _Alignment = b"\xFEG" + bytes(chr(index), "utf8")
+        alignment = b"\xFEG" + bytes(chr(index), "utf8")
 
-        if v_align == self._Alignment.TOP:
-            _Alignment += b"\x01"
-        elif v_align == self._Alignment.BOTTOM:
-            _Alignment += b"\x02"
+        if v_align == Alignment.TOP:
+            alignment += b"\x01"
+        elif v_align == Alignment.BOTTOM:
+            alignment += b"\x02"
 
-        return _Alignment + bytes(text, "utf8")
+        return alignment + bytes(text, "utf8")
+
+    def write_bytes(self, text):
+
+        self._dev.write(text)
 
     def _reset_lcd(self):
 
         # Empty LCD screen (X) and send cursor home (H).
-        self._dev.write(b"\xFEX\xFEH")
+        self.write_bytes(b"\xFEX\xFEH")
 
     def _write_header_with_text(self, text=""):
 
         self._reset_lcd()
         self._write_header()
-        self._dev.write(self._align_text(text,
-            self._Alignment.CENTER, self._Alignment.BOTTOM))
+        self.write_bytes(
+            self.align_text2bytes(text, Alignment.CENTER, Alignment.BOTTOM))
 
     def _write_header(self, header="Quod Libet"):
 
-        self._dev.write(self._align_text(header))
+        self.write_bytes(self.align_text2bytes(header))
 
     def PluginPreferences(self, parent):
 
@@ -376,13 +380,13 @@ class MatrixOrbitalLCD(EventPlugin):
         self._reset_lcd()
 
         # Ready horizontal bars.
-        self._dev.write(b"\xFEh")
+        self.write_bytes(b"\xFEh")
         # Turn backlight on.
-        self._dev.write(b"\xFEB\x00")
+        self.write_bytes(b"\xFEB\x00")
 
         self._write_header()
 
-        self._npld = self._NowPlayingLCDData(self)
+        self._npld = NowPlayingLCDData(self)
         self._tracker = TimeTracker(app.player)
         self._tracker.connect('tick', self._npld.on_tracker_tick)
         self._tracker.set_interval(int(CONFIG.lcd_interval))
@@ -396,7 +400,7 @@ class MatrixOrbitalLCD(EventPlugin):
         self._reset_lcd()
 
         # Turn backlight off.
-        self._dev.write(b"\xFEF")
+        self.write_bytes(b"\xFEF")
 
     def plugin_on_seek(self, song, msec):
 
@@ -411,7 +415,7 @@ class MatrixOrbitalLCD(EventPlugin):
 
         # Draw a horizontal bar graph starting at column 1 on row 2
         # to the right (0), with a length of 0-100 pixels.
-        self._dev.write(b"\xFE\x7C\x01\x02\x00" +
+        self.write_bytes(b"\xFE\x7C\x01\x02\x00" +
             bytes(chr(percent_played), "utf8"))
 
     def plugin_on_song_started(self, song):
